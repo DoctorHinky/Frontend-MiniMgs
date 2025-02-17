e23d0942eedd8879c7c2eaa3a4277bdc90516b81
Norman <norman.tetzlaff@gmail.com>
2025-02-17 22:58:28 +0100

    change the app, to see the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <></>;
-}
+import ChatInput from "./Components/ChatInput";
+import ChatWindow from "./Components/ChatWindow";
+import ClientList from "./Components/ClientList";
+import { WebSocketProvider } from "./Context/WebSocketContext";
 
-export default App;
+export default function App() {
+  return (
+    <WebSocketProvider>
+      <div className="app">
+        <h1>Annonymer Chat</h1>
+        <ClientList />
+        <ChatWindow />
+        <ChatInput />
+      </div>
+    </WebSocketProvider>
+  );
+}
