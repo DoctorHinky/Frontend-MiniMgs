d3304c626c2ea2c06173964f30e677a5a6002148
Norman <norman.tetzlaff@gmail.com>
2025-02-17 22:34:51 +0100

    add Context
    den Context geschrieben

---
+++ src/Context/WebSocketContext.jsx
@@ -0,0 +1,83 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { WebsocketReducer, initState } from "./WebSocketReducer";
+import { error, log, warn } from "console";
+import PropTypes from "prop-types";
+
+const WebSocketContext = createContext();
+// only for dev purposes
+const webSocket_URL = "ws://localhost:8080/ws";
+
+export function WebSocketProvider({ children }) {
+  const [state, dispatch] = useReducer(WebsocketReducer, initState);
+
+  useEffect(() => {
+    const ws = new WebSocket(webSocket_URL);
+
+    ws.addEventListener("open", () => {
+      log("Connected to server");
+      dispatch({ type: "SET_WS", payload: ws });
+    });
+
+    ws.addEventListener("message", (e) => {
+      let data;
+      try {
+        data = JSON.parse(e.data);
+      } catch (err) {
+        error("Failed to parse incoming message", err);
+      }
+
+      switch (data.type) {
+        case "WELCOME":
+          dispatch({ type: "SET_CLIENT_ID", payload: data.clientId });
+          break;
+
+        case "NORMAL_MESSAGE":
+          dispatch({ type: "ADD_MESSAGE", payload: data });
+          break;
+
+        case "PRIVATE_MESSAGE":
+          dispatch({ type: "ADD_MESSAGE", payload: data });
+          break;
+
+        case "LOCK_LOBBY":
+          dispatch({ type: "SET_LOBBY_LOCKED", payload: true });
+          break;
+
+        case "UNLOCK_LOBBY":
+          dispatch({ type: "SET_LOBBY_LOCKED", payload: false });
+          break;
+
+        case "NEW_HOST":
+          dispatch({ type: "SET_CLIENT_ID", payload: "Host" });
+          break;
+
+        default:
+          warn("unbekannter Nachrichten Type:", data.type);
+          break;
+      }
+    });
+
+    ws.addEventListener("close", () => {
+      log("Disconnected from server");
+      dispatch({ type: "SET_WS", payload: null });
+    });
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+    <WebSocketContext.Provider value={{ state, dispatch }}>
+      {children}
+    </WebSocketContext.Provider>
+  );
+}
+
+export function useWebSocket() {
+  return useContext(WebSocketContext);
+}
+
+WebSocketProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
