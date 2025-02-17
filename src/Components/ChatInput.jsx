6768296827237c2ae23f11929e7a1e3600638f9a
Norman <norman.tetzlaff@gmail.com>
2025-02-17 22:43:49 +0100

    ChatInput Comp geschrieben

---
+++ src/Components/ChatInput.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useWebSocket } from "../Context/WebSocketContext";
+
+export default function ChatInput() {
+  const [message, setMessage] = useState("");
+  const [isPrivate, setIsPivate] = useState(false);
+  const { state } = useWebSocket();
+  const [receiverId, setReceiverId] = useState("");
+
+  const sendMessage = () => {
+    if (state.ws && message.trim() !== "" && isPrivate === false) {
+      state.ws.send(
+        JSON.stringify({
+          type: "NORMAL_MESSAGE",
+          payload: {
+            clientId: state.clientId,
+            message,
+          },
+        })
+      );
+      setMessage("");
+    }
+
+    if (
+      state.ws &&
+      message.trim() !== "" &&
+      isPrivate === true &&
+      receiverId !== ""
+    ) {
+      state.ws.send(
+        JSON.stringify({
+          type: "PRIVATE_MESSAGE",
+          payload: {
+            clientId: state.clientId,
+            receiver: receiverId,
+            message,
+          },
+        })
+      );
+      setMessage("");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            sendMessage();
+          }
+        }}
+      />
+      <button onClick={sendMessage}>Send</button>
+      <input
+        type="checkbox"
+        checked={isPrivate}
+        onChange={(e) => setIsPivate(e.target.checked)}
+      />
+      <input
+        type="text"
+        value={receiverId}
+        onChange={(e) => setReceiverId(e.target.value)}
+        placeholder="Receiver ID"
+      />
+    </div>
+  );
+}
